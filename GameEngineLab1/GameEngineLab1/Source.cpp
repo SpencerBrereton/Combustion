847980d455cb9e41c5b4ecbe26d44f138a1e4a79
Spencer Brereton <spencer.brereton@hotmail.com>
2017-10-09 12:17:47 -0400

    Lab 2 and 3 (Incomplete)

--- GameEngineLab1/GameEngineLab1/Source.cpp
+++ GameEngineLab1/GameEngineLab1/Source.cpp
@@ -1,5 +1,77 @@
+﻿$ echo | gcc - Wp, -v - x c++ - -fsyntax - only
+#include <Windows.h>
 
 int main() {
+	IsOnlyInstance();
+	CheckStorage();
+	CheckMemory();
+	ReadCPUSpeed();
+}
 
-	return 0;
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded) {
+	// Check for enough free disk space on the current disk.
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree;
+	_getdiskfree(drive, &diskfree);
+	unsigned __int64 const neededClusters = diskSpaceNeeded /
+		(diskfree.sectors_per_cluster*diskfree.bytes_per_sector);
+	if (diskfree.avail_clusters < neededClusters) {
+		// if you get here you don’t have enough disk space!
+		GCC_ERROR("CheckStorage Failure : Not enough physical storage.");
+		return false;
+	}
+	return true;
+}
+
+bool IsOnlyInstance(LPCTSTR gameTitle) {
+	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+	if (GetLastError() != ERROR_SUCCESS) {
+		HWND hWnd = FindWindow(gameTitle, NULL);
+		if (hWnd) {
+			// An instance of your game is already running.
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			SetFocus(hWnd);
+			SetForegroundWindow(hWnd);
+			SetActiveWindow(hWnd);
+			return false;
+		}
+	}
+	return true;
+}
+
+bool CheckMemory(const DWORDLONG physicalRAMNeeded, const
+	DWORDLONG virtualRAMNeeded) {
+	MEMORYSTATUSEX status;
+	GlobalMemoryStatusEx(&status);
+	if (status.ullTotalPhys < physicalRAMNeeded) {
+		GCC_ERROR("CheckMemory Failure : Not enough physical memory."); return false;
+	}
+	// Check for enough free memory.
+	if (status.ullAvailVirtual < virtualRAMNeeded) {
+		GCC_ERROR("CheckMemory Failure : Not enough virtual	memory.");
+		return false;
+	}
+	char *buff = GCC_NEW char[virtualRAMNeeded];
+	if (buff)
+		delete[] buff;
+	else {
+		// even though there is enough memory, it isn’t
+		GCC_ERROR("CheckMemory Failure : Not enough contiguous memory.");
+		return false;
+	}
+}
+DWORD ReadCPUSpeed() {
+	DWORD BufSize = sizeof(DWORD);
+	DWORD dwMHz = 0;
+	DWORD type = REG_DWORD;
+	HKEY hKey;
+	// open the key where the proc speed is hidden:
+	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,KEY_READ, &hKey);
+	if (lError == ERROR_SUCCESS) {
+		// query the key:
+		RegQueryValueEx(hKey, L"~MHz", NULL, &type, (LPBYTE)
+			&dwMHz, &BufSize);
+	}
+	return dwMHz;
 }
